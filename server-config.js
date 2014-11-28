7bebbad5d2247d8784681f43a7adbbfba0e676f6
Cory Asato <czasato@gmail.com>
2014-11-28 13:08:56 -0800

    Updated to MongoDB && setup Azure remote

--- server-config.js
+++ server-config.js
@@ -12,7 +12,7 @@ app.configure(function() {
   app.use(partials());
   app.use(express.bodyParser());
   app.use(express.static(__dirname + '/public'));
-  app.use(express.cookieParser('shhhh, very secret'));
+  app.use(express.cookieParser('hushmydarling'));
   app.use(express.session());
 });
 
